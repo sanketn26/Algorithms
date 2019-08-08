c5dbe4a75ec0a5474b0c7c1be94aa52f7d3a9430
Sanket Naik <sanketn@gmail.com>
2019-08-08 22:41:16 +0530

    Quick Find

--- Week1/week1/src/main/java/org/sanket/algorithms/unionfind/QuickFind.java
+++ Week1/week1/src/main/java/org/sanket/algorithms/unionfind/QuickFind.java
@@ -4,9 +4,11 @@ import org.sanket.algorithms.unionfind.UnionFind;
 
 public class QuickFind implements UnionFind {
     private int[] nodes;
+    private int nodeSize;
 
     public QuickFind(int nodeSize){
         nodes = new int[nodeSize];
+        this.nodeSize = nodeSize;
         for(int i = 0; i < nodeSize; i++){
             nodes[i] = i;
         }
@@ -14,6 +16,21 @@ public class QuickFind implements UnionFind {
 
     public int[] getNodes(){
         return nodes;
+    }
+
+    @Override
+    public void union(int target, int source) {
+        int val_at_target = nodes[target];
+        int val_at_source = nodes[source];
+        for(int i = 0; i < nodeSize; i++){
+            if(nodes[i] == val_at_target)
+                nodes[i] = val_at_source;
+        }
+    }
+
+    @Override
+    public boolean isconnected(int target, int source) {
+        return nodes[target] == nodes[source];
     }
 
 }
--- Week1/week1/src/test/java/org/sanket/algorithms/QuickFindTest.java
+++ Week1/week1/src/test/java/org/sanket/algorithms/QuickFindTest.java
@@ -2,8 +2,6 @@ package org.sanket.algorithms;
 
 import static org.junit.Assert.assertTrue;
 
-import java.beans.Transient;
-
 import org.junit.Test;
 import org.sanket.algorithms.unionfind.QuickFind;
 import org.sanket.algorithms.unionfind.UnionFind;
@@ -15,16 +13,32 @@ public class QuickFindTest{
         assertTrue(qf != null);
     }
 
+    @Test
     public void checkInitiatlization(){
         QuickFind qf = new QuickFind(10);
         int[] nodes = qf.getNodes();
         for(int i = 0; i < 10; i++){
             if(nodes[i] != i){
-                assertTrue(False);
+                assertTrue(false);
                 break;
             }
         }
-        assertTrue(True);
+        assertTrue(true);
     }
 
+    @Test
+    public void unionFindTest(){
+        UnionFind uf = new QuickFind(10);
+        uf.union(0, 1);
+        uf.union(2, 3);
+        uf.union(2, 0);
+        uf.union(4, 7);
+        uf.union(8, 5);
+        uf.union(6, 4);
+        uf.union(4, 9);
+        assertTrue(uf.isconnected(7, 9));
+        assertTrue(uf.isconnected(0, 3));
+        assertTrue(uf.isconnected(8, 5));
+        assertTrue(!uf.isconnected(5, 9));
+    }
 }
